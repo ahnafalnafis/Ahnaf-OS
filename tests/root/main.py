6cef6b8e198f6e14a38ff02d786e6ead7e7287c2
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2021-08-12 21:28:07 +0600

    Added environment variable feature

--- tests/root/main.py
+++ tests/root/main.py
@@ -14,25 +14,24 @@ bin_dir = root_dir+"bin/"
 binaries = os.listdir(bin_dir)
 
 
-def read_PATH(paths):
-    dirs = list()
-    scripts = list()
-    for path in paths:
-        path = path[0].replace("/", root_dir) + path[1:]
-        dirs.append(path)
-        for dir in os.listdir(path):
-            scripts.append(dir)
-    return scripts
-
-
 def main(username):
     while True:
+        found = False
         command = input(f"[{username}@{hostname} /]$ ")
-        if command == "shutdown":
-            exit("Shutting Down...")
+        scripts = list()
+        for path in paths:
+            path = path[0].replace("/", root_dir) + path[1:]
+            for dir in path.split():
+                for item in os.listdir(dir):
+                    scripts.append(item)
 
-        elif command.split()[0] in read_PATH(paths):
-            os.system(f"python {bin_dir}{command}")
+                if command == "shutdown":
+                    exit("Shutting Down...")
+
+                elif command.split()[0] in scripts:
+                    os.system(f"python {path}/{command}")
+                    found = True
 
         else:
-            print(f"Command not found: {command.split()[0]}")
+            if not found:
+                print(f"Command not found: {command.split()[0]}")
